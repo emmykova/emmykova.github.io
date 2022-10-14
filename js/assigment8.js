cd0b5d29dff1bff587dcb93b56d31e95eccc5519
emmy kovalic <emko7159@colorado.edu>
2022-10-13 18:00:19 -0600

    Update assigment8.js

--- js/assigment8.js
+++ js/assigment8.js
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day. ";
-var insertX  = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-var insertY = ["the soup kitchen", "Disneyland", "the White House"];
-var insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+var storyText = "It was 50 fahrenheit outside, so :insertx: went skateboarding. When they got to :inserty:, they tried to do a :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and is amazing at skateboarding. ";
+var insertX  = ["your nextdoor neighbor", "Big Daddy", "that one man from the grocery store"];
+var insertY = ["the edge of town", "Disneyland", "Italy"];
+var insertZ = ["kickflip", "murder flip", "gazelle flip"];
 
 randomize.addEventListener('click', result);
 
