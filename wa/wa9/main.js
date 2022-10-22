069c86e6a5dc50ff09d5735920156428254cd3c4
emmy kovalic <emko7159@colorado.edu>
2022-10-21 23:34:51 -0600

    wa9

---
+++ wa/wa9/main.js
@@ -0,0 +1,71 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+const arrayOfImg = [
+    {image: 'images/pic1.jpg'}, 
+    {image: 'images/pic2.jpg'},
+    {image: 'images/pic3.jpg'},
+    {image: 'images/pic4.jpg'},
+    {image: 'images/pic5.jpg'}
+];
+
+/* Declaring the alternative text for each image file */
+const altText = {
+    'images/pic1.jpg': 'Closeup of a human eye',
+    'images/pic2.jpg': 'Rock that looks like a wave',
+    'images/pic3.jpg': 'purple and white flowers',
+    'images/pic4.jpg': 'section of a wall from a pharoah\'s tomb',
+    'images/pic5.jpg': 'moth on a leaf'
+}
+
+/* Looping through images */
+
+for(let i = 0; i < 5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', arrayOfImg[i].image);
+    newImage.setAttribute('alt', altText[i]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+    });
+}
+
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === "dark") {
+      btn.setAttribute('class',"light");
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class',"dark");
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
+
+// btn.addEventListener('click', () => {
+//     if(btn.getAttribute('class') == 'dark') {
+//         btn.setAttribute('class', 'light');
+//         btn.textContent = 'Lighten';
+//         overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+//     }
+//     else {
+//         btn.setAttribute('class', 'dark');
+//         btn.textContent = 'Darken';
+//         overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+//     }
+// });
+
