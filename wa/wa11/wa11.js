3e361104a6a1a90a56e8c5b0032516bd424ea378
emmy kovalic <emko7159@colorado.edu>
2022-11-06 19:52:13 -0700

    wa11

---
+++ wa/wa11/wa11.js
@@ -0,0 +1,31 @@
+//adding new variable for new quote button
+const newQuote = document.querySelector('#js-new-quote');
+//add variable for the api endpoint
+const endpoint = 'https://api.thecatapi.com/v1/images/search';
+
+//creating event listener to know when the button is clicked
+    newQuote.addEventListener('click', getQuote); 
+
+//getQuote function
+async function getQuote() {
+    try {
+        const response = await fetch(endpoint)
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+        const json = await response.json();
+        // console.log(json);
+        displayQuote(json.message);
+      } 
+      catch (err) {
+        console.log(err)
+        alert('Failed to fetch new quote');
+      }
+}
+
+//display quote function
+function displayQuote(quote) {
+    const quoteText = document.querySelector('#js-quote-text');
+    quoteText.textContent = quote;
+  }
+
